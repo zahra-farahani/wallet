6139dd696fa1a4065c48d9dc5063ea35dccc0a4d
zahra-farahani <zahra.shahrabi97@gmail.com>
2025-09-29 12:37:42 +0330

    feat: add user entity + repo

---
+++ src/main/java/com/snapppay/wallet/entity/User.java
@@ -0,0 +1,32 @@
+package com.snapppay.wallet.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String userName;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private boolean active;
+}
